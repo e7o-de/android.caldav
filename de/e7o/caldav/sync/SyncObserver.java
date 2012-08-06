6c1511c63f0845465e4f500995eb38b27f5ab1f8
Sven Herzky <sven@e7o.de>
2012-08-06 20:13:53 +0200

    Repository completion: Removing de dir

--- de/e7o/caldav/sync/SyncObserver.java
+++
@@ -1,7 +0,0 @@
-package de.e7o.caldav.sync;
-
-public interface SyncObserver {
-	public void syncDone(String result);
-	public void syncError(String result);
-	public void syncState(String state);
-}
